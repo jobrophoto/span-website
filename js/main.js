a23f7209dd98e212dddb40a1acc4d6b0426fc6b3
Joe Boucher <jobrophotodesign@gmail.com>
2026-09-19 20:52:53 -0400

    Refactor CSS and HTML structure; enhance JavaScript functionality
    - Updated CSS for consistent formatting and improved readability.
    - Added new "About" section in HTML with corresponding styles.
    - Enhanced JavaScript for better event handling and code clarity.
    - Ensured all image attributes and links are properly formatted.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const whyItems = document.querySelectorAll('.why-item');
-const whyImageDisplay = document.getElementById('why-image-display');
+const whyItems = document.querySelectorAll(".why-item");
+const whyImageDisplay = document.getElementById("why-image-display");
 
-whyItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-        whyItems.forEach(function(el) {
-            el.classList.remove('active');
-        });
-        item.classList.add('active');
-
-        const newImage = item.getAttribute('data-image');
-        whyImageDisplay.src = newImage;
+whyItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    whyItems.forEach(function (el) {
+      el.classList.remove("active");
     });
+    item.classList.add("active");
+
+    const newImage = item.getAttribute("data-image");
+    whyImageDisplay.src = newImage;
+  });
 });
